c0c15a51c27fa7e39173fc8bc4e059e30f412279
mickeyescaleto <mickeyescaleto@gmail.com>
2024-10-01 00:06:14 +0300

    build: configure prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "none",
+};
