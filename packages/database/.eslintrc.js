46e25b66c39aebc6316689aa869df86ec1c72be8
mickeyescaleto <mickeyescaleto@gmail.com>
2024-10-01 15:04:13 +0300

    feat: add a package to interact with the database

---
+++ packages/database/.eslintrc.js
@@ -0,0 +1,17 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ['@draftulse/eslint-config/library.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true
+  },
+  rules: {
+    'turbo/no-undeclared-env-vars': [
+      'error',
+      {
+        allowList: ['NODE_ENV']
+      }
+    ]
+  }
+};
